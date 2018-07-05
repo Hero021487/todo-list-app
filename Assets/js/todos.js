49834a85484f96ecafdb7f03033209bbdb31eeab
Rizalino Requiero <riztreq@gmail.com>
2018-07-05 07:19:06 +0000

    Add CSS and JQuery codes

---
+++ Assets/js/todos.js
@@ -0,0 +1,28 @@
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+// Click on X to Delete Todos
+$("ul").on("click", "span", function(evt){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(evt){
+	if(event.which === 13){
+		// grabbing new to do text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash-alt'></i></span> " + todoText + "</li>")
+
+	}
+});
+
+
+$(".fa-plus-square").click(function(){
+	$("input[type='text']").fadeToggle();
+});
